893321a0e0a0c5418f0f4844e98d9b1f357a10d4
Abisheck B <ramaniabira123@gmail.com>
2025-07-09 18:20:30 +0530

    new files added for setup

--- skitlearn/Classification/knn_classify.py
+++ skitlearn/Classification/knn_classify.py
@@ -4,6 +4,24 @@ from sklearn.model_selection import train_test_split
 from sklearn import neighbors , metrics
 from sklearn.preprocessing import LabelEncoder
 
-cardata = pd.read_csv("car.data")
+cardata = pd.read_csv("skitlearn/Classification/car.data")
 
-print(cardata.head())
+# print(cardata.head())
+
+x = cardata[['buying',
+             'maint',
+             'safety']].values
+
+y = cardata[['class']]
+le = LabelEncoder()
+for i in range(len(x[0])):
+    x[:, i] = le.fit_transform(x[:, i])
+print(x)
+
+y = cardata[['class']].replace({
+    'unacc': 0,
+    'acc': 1,
+    'good': 2,
+    'vgood': 4
+})
+print(y)
